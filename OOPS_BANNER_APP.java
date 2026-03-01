a0cc1d4f3c059417a1be0cfd925f61f085a3a545
ShivamRoyy <sk1463@srmist.edu.in>
2026-03-01 16:54:19 +0530

    Refactor banner lines initialization in OOPS_BANNER_APP

--- OOPS_BANNER_APP.java
+++ OOPS_BANNER_APP.java
@@ -8,19 +8,20 @@ public class OOPS_BANNER_APP {
      * @author Shivam Kumar
      * @version 1.0
      */
+    public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] lines = new String[] {
-            String.join("", " ", "*****", "  ", "*****", "  ", "******", "  ", "*****"),
-            String.join("", "*", "     *", "  ", "*", "     *", "  ", "*", "     *", "  ", "*", "     *"),
-            String.join("", "*", "     *", "  ", "*", "     *", "  ", "*", "     *", "  ", "*", "     *"),
-            String.join("", "*", "     *", "  ", "*", "     *", "  ", "*", "******", "  ", "*****"),
-            String.join("", "*", "     *", "  ", "*", "     *", "  ", "*", "     *", "  ", "*", "     *"),
-            String.join("", "*", "     *", "  ", "*", "     *", "  ", "*", "     *", "  ", "*", "     *"),
-            String.join("", " ", "*****", "  ", "*****", "  ", "*", "     ", "  ", "*****")
-        };
+        String[] lines = new String[5];
+
+        lines[0] = String.join("  ", " *** ", " *** ", "**** ", " ****");
+        lines[1] = String.join("  ", "*   *", "*   *", "*   *", "*    ");
+        lines[2] = String.join("  ", "*   *", "*   *", "**** ", " *** ");
+        lines[3] = String.join("  ", "*   *", "*   *", "*    ", "    *");
+        lines[4] = String.join("  ", " *** ", " *** ", "*    ", "**** ");
+
         for (String line : lines) {
             System.out.println(line);
         }
     }
 }
+ 
 
